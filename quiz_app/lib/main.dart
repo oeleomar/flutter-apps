74197a2005bc41eae49abe379e0ceec6ab42b41f
oeleomar <eleomar.dev@gmail.com>
2025-10-13 16:10:50 -0400

    feat: add start screen

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
 
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                const Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: AlignmentGeometry.topLeft,
+              end: AlignmentGeometry.bottomRight,
+            ),
+          ),
+          child: StartScreen(),
+        ),
+      ),
+    ),
+  );
+}
