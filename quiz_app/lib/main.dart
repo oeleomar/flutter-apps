fcef867a8340d63a37379d23b4989986bbe0a3ae
oeleomar <eleomar.dev@gmail.com>
2025-10-13 17:12:50 -0400

    refactor: all files

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,24 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                const Color.fromARGB(255, 78, 13, 151),
-                Color.fromARGB(255, 107, 15, 168),
-              ],
-              begin: AlignmentGeometry.topLeft,
-              end: AlignmentGeometry.bottomRight,
-            ),
-          ),
-          child: StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(Quiz());
 }
