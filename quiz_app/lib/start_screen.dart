c857dbf37fe4a01a9270bb89c64ff8aa2656d401
oeleomar <eleomar.dev@gmail.com>
2025-10-13 17:02:05 -0400

    fat: add opacity

--- quiz_app/lib/start_screen.dart
+++ quiz_app/lib/start_screen.dart
@@ -12,8 +12,38 @@ class StartScreen extends StatelessWidget {
           Image.asset(
             'assets/images/quiz-logo.png',
             width: 300,
+            color: Color.fromARGB(150, 255, 255, 255),
           ),
-          Text('Quiz '),
+
+          /* Opacity(
+            opacity: 0.6,
+            child: Image.asset(
+              'assets/images/quiz-logo.png',
+              width: 300,
+            ),
+          ), */
+          const SizedBox(
+            height: 80,
+          ),
+
+          const Text(
+            'Aprenda Flutter de maneira divertida!',
+            style: TextStyle(
+              color: Color.fromARGB(255, 237, 223, 252),
+              fontSize: 20,
+            ),
+          ),
+
+          const SizedBox(
+            height: 30,
+          ),
+
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            label: Text('Start Quiz'),
+            icon: Icon(Icons.arrow_right_alt),
+          ),
         ],
       ),
     );
