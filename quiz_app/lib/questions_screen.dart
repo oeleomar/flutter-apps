46e6b4767dca69829b22ce21ef0538e31fca5b8b
oeleomar <eleomar.dev@gmail.com>
2025-10-15 17:15:27 -0400

    feat: add questions and answer button functionality

--- quiz_app/lib/questions_screen.dart
+++ quiz_app/lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -8,8 +9,35 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  onTap() {
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Tela das perguntas");
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Question'),
+          SizedBox(
+            height: 30,
+          ),
+          AnswerButton(
+            text: 'Resposta 1',
+            onTap: onTap,
+          ),
+          AnswerButton(
+            text: 'Resposta 2',
+            onTap: onTap,
+          ),
+          AnswerButton(
+            text: 'Resposta 3',
+            onTap: onTap,
+          ),
+        ],
+      ),
+    );
   }
 }
