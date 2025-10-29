ec165a390e43de55353fa8a3069055a643383047
oeleomar <eleomar.dev@gmail.com>
2025-10-29 14:41:00 -0400

    feat: adicionar formatação de data e ícones de categoria na exibição de despesas

--- expenses_tracker_app/lib/model/expense.dart
+++ expenses_tracker_app/lib/model/expense.dart
@@ -1,8 +1,19 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.MEd();
 
 final uuid = Uuid();
 
 enum Category { comida, viagem, trabalho, espiritualidade }
+
+const categoryIcons = {
+  Category.comida: Icons.lunch_dining,
+  Category.viagem: Icons.flight_takeoff,
+  Category.trabalho: Icons.work,
+  Category.espiritualidade: Icons.mode_night_sharp,
+};
 
 class Expense {
   Expense({
@@ -17,4 +28,8 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category;
+
+  String get formatedDate {
+    return formatter.format(date);
+  }
 }
--- expenses_tracker_app/lib/widgets/expenses_list/expense_item.dart
+++ expenses_tracker_app/lib/widgets/expenses_list/expense_item.dart
@@ -18,6 +18,16 @@ class ExpenseItem extends StatelessWidget {
             Row(
               children: [
                 Text('R\$${expense.amount.toStringAsFixed(2)}'),
+                const Spacer(),
+                Row(
+                  children: [
+                    Icon(categoryIcons[expense.category]),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Text(expense.formatedDate),
+                  ],
+                ),
               ],
             ),
           ],
