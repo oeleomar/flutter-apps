beee98eeea90894ea41acccf67aa7ea575f29845
oeleomar <eleomar.dev@gmail.com>
2025-11-24 16:25:00 -0400

    feat: favorites places: inital commit

---
+++ favorite_places/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final colorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 102, 6, 247),
+  onSurface: const Color.fromARGB(255, 56, 49, 66),
+  brightness: Brightness.dark,
+);
+
+final theme = ThemeData().copyWith(
+  scaffoldBackgroundColor: colorScheme.onSurface,
+  colorScheme: colorScheme,
+  textTheme: GoogleFonts.ubuntuCondensedTextTheme().copyWith(
+    titleSmall: GoogleFonts.ubuntuCondensed(fontWeight: FontWeight.bold),
+    titleMedium: GoogleFonts.ubuntuCondensed(fontWeight: FontWeight.bold),
+    titleLarge: GoogleFonts.ubuntuCondensed(fontWeight: FontWeight.bold),
+  ),
+);
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Great Places',
+      theme: theme,
+      home: Text("Meu lugar favorito!"),
+    );
+  }
+}
