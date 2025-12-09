c974e4f8475f96c283cea85c7d0981fe04293f18
oeleomar <eleomar.dev@gmail.com>
2025-12-09 15:07:22 -0400

    feat: implementar funcionalidade de adição de lugares na tela de adição

--- favorite_places/lib/screens/add_places.dart
+++ favorite_places/lib/screens/add_places.dart
@@ -10,16 +10,40 @@ class AddPlaceScreen extends StatefulWidget {
 }
 
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: Text("Add new Place"),
-            
+      appBar: AppBar(
+        title: Text("Add new Place"),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(12),
+        child: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(labelText: "Title"),
+              controller: _titleController,
+              style: TextStyle(color: Theme.of(context).colorScheme.onSurface),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.add),
+              label: const Text("Add place"),
+            ),
+          ],
         ),
-    body: Column(
-        
-    ),
-    )
+      ),
+    );
   }
 }
--- favorite_places/lib/screens/places.dart
+++ favorite_places/lib/screens/places.dart
@@ -1,3 +1,4 @@
+import 'package:favorite_places/screens/add_places.dart';
 import 'package:favorite_places/widgets/places_list.dart';
 import 'package:flutter/material.dart';
 
@@ -9,7 +10,16 @@ class PlacesScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Your Places"),
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (ctx) => const AddPlaceScreen()),
+              );
+            },
+            icon: Icon(Icons.add),
+          ),
+        ],
       ),
       body: PlacesList(
         places: [],
