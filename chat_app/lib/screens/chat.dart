6eb5736f0dd93fd5f06b89a553b7ee4eccf68f0c
oeleomar <eleomar.dev@gmail.com>
2026-05-22 14:11:18 -0400

    feat: end of course

--- chat_app/lib/screens/chat.dart
+++ chat_app/lib/screens/chat.dart
@@ -3,8 +3,18 @@ import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
